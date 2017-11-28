deac3a2b3d712f92a89b609a0b8ecf254085123b
Jordan Bachelin <jordan.bachelin@gmail.com>
2017-11-28 10:04:58 +0100

    loading data first version

--- src/DataLoading.R
+++ src/DataLoading.R
@@ -1,1 +1,14 @@
-## here we load the reviews needed to classified
+# Projet Text Mining ####
+# Auteur : Jordan BACHELIN ####
+
+rm(list = ls())
+library(XML)
+
+# Chargement des données ####
+
+xmldoc <- xmlParse("data/train_2017.xml")
+rootNode <- xmlRoot(xmldoc)
+rootNode[1]
+data <- xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
+cd.catalog <- data.frame(t(data),row.names=NULL)
+cd.catalog[1:2,]
