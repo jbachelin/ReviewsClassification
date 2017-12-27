50997a1a850ddb19fd72b39e19c0c10a997b4b67
Jordan Bachelin <jordan.bachelin@gmail.com>
2017-12-27 16:52:58 +0100

    change data loading + stats on data

--- src/DataLoading.R
+++ src/DataLoading.R
@@ -1,30 +1,68 @@
 # Projet Text Mining ####
-# Auteur : Jordan BACHELIN ####
+# Auteurs : Jordan BACHELIN & Eric HAMMEL ####
 
 rm(list = ls())
 library(XML)
+library(dplyr)
 
 # Chargement des données ####
 
 data <- xmlParse("data/train_2017.xml")
 xml_data <- xmlToList(data)
 
-reviews <- matrix(0, length(xml_data))
-target <- matrix(0, length(xml_data))
-category <- matrix(0, length(xml_data))
-polarity <- matrix(0, length(xml_data))
-from <- matrix(0, length(xml_data))
-to <- matrix(0, length(xml_data))
+# reviews <- matrix(0, length(xml_data))
+# target <- matrix(0, length(xml_data))
+# category <- matrix(0, length(xml_data))
+# polarity <- matrix(0, length(xml_data))
+# from <- matrix(0, length(xml_data))
+# to <- matrix(0, length(xml_data))
 
-for (cell in 1:length(xml_data))
-{
-  reviews[cell] <- xml_data[[cell]]$sentences$sentence$text
-  opinions <- as.list(xml_data[[cell]]$sentences$sentence$Opinions$Opinion)
-  target[cell] <- if(is.null(opinions$target)) NA else opinions$target
-  category[cell] <- if(is.null(opinions$category)) NA else opinions$category
-  polarity[cell] <- if(is.null(opinions$polarity)) "neutral" else opinions$polarity
-  # from[cell] <- opinions$from
-  # to[cell] <- opinions$to
+filldf <- function(xml_data, opinions){
+  for (cell in 1:length(xml_data))
+  {
+    for (idx in 1:length(xml_data[[cell]]$sentences)) {
+      review <- as.character(xml_data[[cell]]$sentences[[idx]]$text)
+      if (length(xml_data[[cell]]$sentences[[idx]]) == 2) {
+        opinions[nrow(opinions) + 1, ] <- c(review, NA, NA, NA, NA, NA, as.numeric(cell))
+      } else if (length(xml_data[[cell]]$sentences[[idx]]$Opinions) > 1) {
+        # Pour l'instant on ne considere pas les avis ayant plusieurs polarites  
+        break
+        # Code permettant de les traiter plus tard
+          # for (opinion in 1:length(xml_data[[cell]]$sentences[[idx]]$Opinions)) {
+          #   opinions[nrow(opinions) + 1, ] <- c(review,
+          #                                       as.character(xml_data[[cell]]$sentences[[idx]]$Opinions[opinion]$Opinion[[1]]),
+          #                                       as.character(xml_data[[cell]]$sentences[[idx]]$Opinions[opinion]$Opinion[[2]]),
+          #                                       as.character(xml_data[[cell]]$sentences[[idx]]$Opinions[opinion]$Opinion[[3]]),
+          #                                       as.numeric(xml_data[[cell]]$sentences[[idx]]$Opinions[opinion]$Opinion[[4]]),
+          #                                       as.numeric(xml_data[[cell]]$sentences[[idx]]$Opinions[opinion]$Opinion[[5]]),
+          #                                       as.numeric(cell))
+          #}
+      } else {
+        opinions[nrow(opinions) + 1, ] <- c(review,
+                                            as.character(xml_data[[cell]]$sentences[[idx]]$Opinions$Opinion[[1]]),
+                                            as.character(xml_data[[cell]]$sentences[[idx]]$Opinions$Opinion[[2]]),
+                                            as.character(xml_data[[cell]]$sentences[[idx]]$Opinions$Opinion[[3]]),
+                                            as.numeric(xml_data[[cell]]$sentences[[idx]]$Opinions$Opinion[[4]]),
+                                            as.numeric(xml_data[[cell]]$sentences[[idx]]$Opinions$Opinion[[5]]),
+                                            as.numeric(cell))
+      }
+    }
+  }
+  opinions
 }
 
-opinions <- data.frame(reviews, target, category, polarity, from, to)
+opinions <- data.frame(rewiew = character(0),
+                       target = character(0),
+                       category = character(0),
+                       polarity = character(0),
+                       from = numeric(),
+                       to = numeric(),
+                       index = numeric(),
+                       stringsAsFactors = FALSE)
+
+opinions <- filldf(xml_data, opinions)
+opinions <- opinions[, -2] # On retire la variable "target"
+opinions <- opinions[-which(opinions$polarity == "neutral" | is.na(opinions$polarity)),] # On ne prend pas en consideration les polarites neutres ou manquantes
+
+# test <- opinions
+# test <- aggregate(. ~ test$rewiew, test, list)
---
+++ src/DataStatistics.R
@@ -0,0 +1,59 @@
+# Projet Text Mining ####
+# Auteurs : Jordan BACHELIN & Eric HAMMEL ####
+
+# Statistiques sur le jeu de données
+
+source("src/DataLoading.R")
+library("wordcloud")
+library("RColorBrewer")
+library("tm")
+library("NLP")
+library(SnowballC)
+
+length(unique(opinions$rewiew)) # 542 avis differents dans le jeu d'entrainement après filtrage dont :
+
+length(which(opinions$polarity == "positive")) # 408 contenant un avis positif
+length(which(opinions$polarity == "negative")) # 136 contenant un avis negatif
+# length(which(opinions$polarity == "neutral")) # [...] contenant un avis neutre
+
+# length(unique(opinions$target)) # 723 cibles differentes dans le jeu d'entrainement (y compris la cible NULL)
+length(unique(opinions$category)) # 12 catégories d'avis differentes dans le jeu d'entrainement (y compris NA)
+
+# Pretraitement des avis
+reviews <- as.matrix(unique(opinions$rewiew))
+review.corpus <- Corpus(VectorSource(reviews))
+review.corpus <- tm_map(review.corpus, content_transformer(tolower)) # Mise en minuscule
+review.corpus <- tm_map(review.corpus, removeNumbers) # Suppression des nombres
+review.corpus <- tm_map(review.corpus, removePunctuation) # Suppression de la ponctuation
+inspect(review.corpus[1])
+
+# Approche fréquentiste des mots presents dans les avis (sans mots-outils seulement)
+review.corpus.sw <- tm_map(review.corpus, removeWords, stopwords('english'))
+message("Review with stopwords:")
+review.corpus[[1]]$content
+message("Review without stopwords:")
+review.corpus.sw[[1]]$content
+review.dtm.sw <- TermDocumentMatrix(review.corpus.sw)
+m.sw <- as.matrix(review.dtm.sw)
+review.freq.sw <- rowSums(m.sw)
+review.freq.sw.sorted <- sort(review.freq.sw, decreasing = TRUE)
+# Histogramme representant les 50 premiers mots les plus frequents dans le corpus total
+barplot(review.freq.sw.sorted[1:50], xlab = "Word", ylab = "Frequency", las = 2)
+# Nuage des mots du corpus (plus ou moins gros en fonction de leur frequence)
+wordcloud(review.corpus.sw, max.words = 200, colors = brewer.pal(8, "Dark2"))
+
+# Approche fréquentiste des mots presents dans les avis (avec stemming et TF/IDF cette fois)
+review.corpus.stemmed <- tm_map(review.corpus.sw, stemDocument, language = "porter")
+message("Review without stemming:")
+review.corpus.sw[[1]]$content
+message("Review with stemming:")
+review.corpus.stemmed[[1]]$content
+review.dtm.stemmed.tfidf = TermDocumentMatrix(review.corpus.stemmed, control = list(weighting = weightTfIdf))
+m.stemmed.tfidf <- as.matrix(review.dtm.stemmed.tfidf)
+# Ou pour plus de lisibilite
+review.freq.stemmed.tfidf <- rowSums(m.stemmed.tfidf)
+review.freq.stemmed.tfidf.sorted <- sort(review.freq.stemmed.tfidf, decreasing = TRUE)
+# Histogramme representant les 50 premiers mots les plus frequents dans le corpus total
+barplot(review.freq.stemmed.tfidf.sorted[1:50], xlab = "Word", ylab = "Frequency", las = 2)
+# Nuage des mots du corpus (plus ou moins gros en fonction de leur frequence)
+wordcloud(review.corpus.stemmed, max.words = 200, colors = brewer.pal(8, "Dark2"))
