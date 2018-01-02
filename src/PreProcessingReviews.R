b3d45db879d97e0ed899744fd254dc35b4ed2526
Eric <eric.hammel01@laposte.net>
2018-01-02 17:57:24 +0100

    adding the beginning of a lemmatization function

--- src/PreProcessingReviews.R
+++ src/PreProcessingReviews.R
@@ -20,9 +20,18 @@ CleaningReviews <- function(dataset, RemoveSw = FALSE, StemDoc = FALSE)
   review.corpus <- tm_map(review.corpus, content_transformer(tolower)) # Mise en minuscule
   review.corpus <- tm_map(review.corpus, removeNumbers) # Suppression des nombres
   review.corpus <- tm_map(review.corpus, removePunctuation) # Suppression de la ponctuation
-  if (isTRUE(RemoveSw)) review.corpus <- tm_map(review.corpus, removeWords, stopwords('english'))
+
+  if (isTRUE(RemoveSw)) review.corpus <- tm_map(review.corpus,
+                                                removeWords,
+                                                stopwords('english'))
   # l'algorithme de porter est utilisé pas défaut
-  if (isTRUE(StemDoc)) review.corpus <- tm_map(review.corpus, stemDocument, language = "english")
+  if (isTRUE(StemDoc)) review.corpus <- tm_map(review.corpus, wordStem, language = "english")
 
     return(review.corpus)
+}
+
+LemmatizeReviews <- function(corpus)
+{
+  library("wordnet")
+  
 }
